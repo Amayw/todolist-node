8eaa50034e464511bfc4be9a3c921b6910d07e21
wywang <727936199@qq.com>
2020-12-09 20:16:30 +0800

    完成清空任务，以及展示任务。同时使用inquirer 库，让用户可以与列表进行交互

--- cli.js
+++ cli.js
@@ -2,14 +2,22 @@ const { program } = require('commander');
 const api =require('./index.js')
 program.version('0.0.1');
 
-program.option('-x, --xxx', 'output')
-
 program
     .command('add')
     .description('add a new task into todoList')
     .action((...args) => {
-        const words=args[1].join(' ');
-        api.add(words);
+        let words;
+        try{
+            words=args[1].join(' ');
+        }catch{
+            console.log('请检查是否输入任务名');
+            return;
+        }
+        api.add(words).then( ()=>{
+            console.log('添加成功');
+        } ,()=>{
+            console.log('添加失败');
+        });
     });
 
 
@@ -17,10 +25,20 @@ program
     .command('clear')
     .description('clear all tasks')
     .action(() => {
-        console.log(`clear all`);
+        api.clear().then(()=>{
+            console.log('清空成功');
+        } ,()=>{
+            console.log('清空失败');
+        });
     });
 
 
+program
+    .command('show')
+    .description('show all tasks')
+    .action(() => {
+        api.showAll();
+    });
 
-program.parse(process.argv);
+    program.parse(process.argv);
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const db = require('./db.js');
+const inquirer = require('inquirer');
 
 module.exports.add = async (title) => {
     const list = await db.read();
@@ -6,4 +7,84 @@ module.exports.add = async (title) => {
     await db.write(list);
 };
 
+module.exports.clear = async () => {
+    await db.write([]);
+};
 
+module.exports.showAll = async () => {
+    const list = await db.read();
+    // list.forEach((task,index)=>{
+    //     newlist.push(`${task.done?'[x]':'[_]'} ${index+1}-${task.title}`);
+    // })
+
+    inquirer
+        .prompt(
+            {
+                type: 'list',
+                name: 'index',
+                message: '请选择你想操作的任务',
+                choices: [{name:'退出',value:'-1'},...list.map((task,index)=>{
+                    return {name:`${task.done?'[x]':'[_]'} ${index+1}-${task.title}`,value:index.toString()}
+                }),{name:'新建任务',value:'-2'}]
+            })
+        .then(answer => {
+            let index=parseInt(answer.index);
+            if(index>=0){
+                //选中任务
+                inquirer
+                    .prompt(
+                        {
+                            type: 'list',
+                            name: 'action',
+                            message: '请选择你想做的操作',
+                            choices: [{name:'退出',value:'quit'},
+                                      {name:'已完成',value:'markAsDone'},
+                                      {name:'未完成',value:'markAsUndone'},
+                                      {name:'改标题',value:'rename'},
+                                      {name:'删除',value:'remove'}
+                            ]
+                        })
+                    .then(answer2=>{
+                        switch(answer2.action){
+                            case 'markAsDone':
+                                list[index].done=true;
+                                db.write(list);
+                                break;
+                            case 'markAsUndone':
+                                list[index].done=false;
+                                db.write(list);
+                                break;
+                            case 'remove':
+                                list.splice(-1,1)
+                                db.write(list);
+                                break;
+                            case 'rename':
+                                inquirer.prompt({
+                                    type: 'input',
+                                    name: 'newTitle',
+                                    message: "请输入新的任务名",
+                                    default:list[index].title
+                                }).then((answer3) => {
+                                    list[index].title=answer3.newTitle
+                                    db.write(list);
+                                });
+                                break;
+                        }
+                    })
+            }else if(index===-1){
+                 //退出
+                return;
+            }else if(index===-2){
+                //新建
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'newTask',
+                    message: "请输入任务名"
+                }).then((answer4) => {
+                    list.push({title:answer4.newTask,done:false})
+                    db.write(list);
+                });
+            }
+        })
+        .catch()
+};
